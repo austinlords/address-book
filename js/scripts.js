170314c9788b46c69b95a0863a0506c453c192b3
Austin Lords <austinlords@outlook.com>
2019-02-15 18:04:13 -0800

    most of back-end and some basic front-end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,69 @@
+var contactCounter = 0;
+
+// Business Logic for AddressBook  -------------
+function AddressBook() {
+  this.contacts = [],
+  this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+// Business Logic for Contacts -------------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
 }
 
+
 Contact.prototype.fullName = function(){
   return this.firstName + " " + this.lastName;
 }
+
+//Front-end Logic
+$(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    contactCounter += 1;
+    var contact = "contact" + String(contactCounter);
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var phoneNumber = $("#phoneNumber").val();
+    var addressBook = new AddressBook();
+    var contact = new Contact(firstName, lastName, phoneNumber);
+    console.log(contact);
+    addressBook.addContact(contact);
+    console.log(addressBook);
+  })
+});
