c94f8a55e965f4667ed8dc16858c3b57a36c8156
Austin Lords <austinlords@outlook.com>
2019-02-18 12:28:20 -0800

    enter email address or physical address

--- js/scripts.js
+++ js/scripts.js
@@ -39,12 +39,26 @@ AddressBook.prototype.deleteContact = function(id) {
   return false;
 }
 
+// Business Logic for Address ------------
+function Address(email, street, state, zip) {
+  this.email = email;
+  this.street = street;
+  this.state = state;
+  this.zip = zip;
+}
+
 // Business Logic for Contacts -------------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, address) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.address = address;
 }
+
+// function Address(emailAddress, physicalAddress) {
+//   this.emailAddress = emailAddress;
+//   this.physicalAddress = physicalAddress;
+// }
 
 Contact.prototype.fullName = function(){
   return this.firstName + " " + this.lastName;
@@ -68,6 +82,7 @@ function showContact(contactID) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
+  $(".address").html(contact.address.email + "<br>" + contact.address.street + "  " + contact.address.state + "  " + contact.address.zip);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
@@ -84,18 +99,39 @@ function attachContactListeners() {
   });
 };
 
+function toggleAddressForm() {
+  $("#email-form").toggleClass("hidden");
+  $("#physical-address-form").toggleClass("hidden");
+}
+
+function attachAddressListeners() {
+  $("form").on("click", ".toggle-address", function() {
+    toggleAddressForm();
+  });
+};
+
 //when document is ready
 $(function() {
   attachContactListeners();
+  attachAddressListeners();
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var inputtedEmailAddress = $("input#new-email-address").val();
+    var inputtedStreetAddress = $("input#new-street-address").val();
+    var inputtedStateAddress = $("input#new-state-address").val();
+    var inputtedZipAddress = $("input#new-zip-address").val();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    $("input#new-email-address").val("");
+    $("input#new-street-address").val("");
+    $("input#new-state-address").val("");
+    $("input#new-zip-address").val("");
+    var newAddress = new Address(inputtedEmailAddress, inputtedStreetAddress, inputtedStateAddress, inputtedZipAddress);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, newAddress);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
    })
